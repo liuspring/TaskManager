b2d08fc851ee564a3846a5a34b297ddc7d9ce55a
liujian <beifeng>
2016-07-12 18:10:20 +0800

    签入节点。

---
+++ TaskManager.HubService/HubMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager.HubService
+{
+    public partial class HubMain : Form
+    {
+        public HubMain()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void HubMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
