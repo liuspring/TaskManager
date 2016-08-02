218dfc1310b9cdc1d731e106ec463fdaa859af83
liujian <beifeng>
2016-08-02 18:17:41 +0800

    添加MiniProfiler性能监控

--- TaskManager.Web/Global.asax.cs
+++ TaskManager.Web/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Abp.Web;
 using Castle.Facilities.Logging;
+using StackExchange.Profiling;
 
 namespace TaskManager.Web
 {
@@ -10,6 +11,20 @@ namespace TaskManager.Web
         {
             AbpBootstrapper.IocManager.IocContainer.AddFacility<LoggingFacility>(f => f.UseLog4Net().WithConfig("log4net.config"));
             base.Application_Start(sender, e);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)//这里是允许本地访问启动监控,可不写
+            {
+                MiniProfiler.Start();
+
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
         }
     }
 }
--- TaskManager.Web/Readme.txt
+++ TaskManager.Web/Readme.txt
@@ -5,3 +5,7 @@ SHOW VARIABLES LIKE '%max_allowed_packet%';
 SET GLOBAL max_allowed_packet = 2*1024*1024*10
 
 https://github.com/CrazyJson/TaskManager.git
+
+MVC MiniProfiler
+http://www.cnblogs.com/GuZhenYin/p/5408632.html
+http://www.cnblogs.com/zhaopei/p/5721789.html#autoid-0-0
